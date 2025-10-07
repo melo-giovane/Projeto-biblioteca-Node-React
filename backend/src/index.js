bfdb0ad70505f07066f6ae0e090062463067a48d
Giovane <giovanec.melo@hotmail.com>
2025-10-07 01:29:15 -0300

    preparo do express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,12 @@
 import dotenv from "dotenv";
+import app from "./app.js";
 
 dotenv.config({
     path: "./.env",
+});
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`server running on http://localhost:${port}`);
 });
