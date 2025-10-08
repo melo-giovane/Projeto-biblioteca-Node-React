a20099a699079d6d155912ab5e1643e429a41a10
Giovane <giovanec.melo@hotmail.com>
2025-10-07 23:36:34 -0300

    feat: add user roles and book status constants

---
+++ backend/src/utils/constants.js
@@ -0,0 +1,15 @@
+export const UserRolesEnum = {
+  ADMIN: "admin",
+  FUNCIONARIO: "funcionario",
+  USUARIO: "usuario"
+}
+
+export const AvailableUserRole = Object.values(UserRolesEnum)
+
+export const BookStatusEnum = {
+  DISPONIVEL: "disponivel",
+  LOCADO: "locado",
+  EM_ATRASO: "em_atraso"
+}
+
+export const BookStatuses = Object.values(BookStatusEnum)
